c5244e5e13b08ea9d979d040e02e85d39110c824
Harpreet Singh <hsingh150109@gmail.com>
2016-11-16 13:23:45 +0530

    corrected num_votes error

--- votecount.sh
+++ votecount.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/bash
-if [ $1 -gt 1 ]
+if [ $1 -gt 0 ]
 then
     sed -i '6d' sql_setup.sql
-    for i in $(seq 1 $1); do sed  -i "$((i + 4)) a create table ballot (username VARCHAR(50) PRIMARY KEY, vote_$((i - 1)) VARCHAR(40));  " sql_setup.sql ; done
+    ballot="create table ballot (username VARCHAR(50) PRIMARY KEY"
+    for i in $(seq 1 $1); do 
+    		temp=", vote_$((i - 1)) VARCHAR(40)"
+   			ballot=$ballot$temp
+		done
+	temp=");"
+    ballot=$ballot$temp
+    sed  -i "5 a $ballot" sql_setup.sql
+    sed -i -E "s/const Number\_of\_votes int \=[0-9]+/const Number\_of\_votes int \=$1/g" election_conf.go
 fi
